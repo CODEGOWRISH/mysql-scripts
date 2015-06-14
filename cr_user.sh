914f7b323393a81bf5090ca956e125281b55e639
gowrishdba <gowrishdba@yahoo.com>
2015-06-14 15:26:23 +0530

    Create cr_user.sh

---
+++ cr_user.sh
@@ -0,0 +1,5 @@
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+CREATE USER 'user' IDENTIFIED BY 'password';
+CREATE USER 'user'@'192.168.0.%' IDENTIFIED BY 'password';
+CREATE USER 'user'@'192.168.0.1/255.255.255.0' IDENTIFIED BY 'password';
