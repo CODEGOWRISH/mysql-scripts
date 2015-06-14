61d74d456e67edeca28c661ae5f74afd68efda59
gowrishdba <gowrishdba@yahoo.com>
2015-06-14 16:03:48 +0530

    Update cr_user.sql

--- cr_user.sql
+++ cr_user.sql
@@ -1,3 +1,6 @@
+rem https://dev.mysql.com/doc/refman/5.1/en/create-user.html
+rem https://dev.mysql.com/doc/refman/5.1/en/account-names.html
+
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
 CREATE USER 'user'@'%' IDENTIFIED BY 'password';
 CREATE USER 'user' IDENTIFIED BY 'password';
